da2ddd558d5c9fad42696f87bec349af024d9190
TharunLavudya <ltharun460@gmail.com>
2026-02-03 23:34:07 -0800

    Created base folder structure for the App.

---
+++ Food-Recipe-App-iOS/Food-Recipe-App-iOS/App/Food_Recipe_App_iOSApp.swift
@@ -0,0 +1,20 @@
+//
+//  Food_Recipe_App_iOSApp.swift
+//  Food-Recipe-App-iOS
+//
+//  Created by rentamac on 2/3/26.
+//
+
+import SwiftUI
+import CoreData
+
+@main
+struct Food_Recipe_App_iOSApp: App {
+    
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
